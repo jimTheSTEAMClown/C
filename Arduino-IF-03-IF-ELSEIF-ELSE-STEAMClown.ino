98279a886ee171feb9bb50cb2b865c08fe731b64
Jim Burnham <jim.the.steam.clown@gmail.com>
2019-09-09 11:14:44 -0700

    Create Arduino-IF-03-IF-ELSEIF-ELSE-STEAMClown.ino

---
+++ Arduino-IF-03-IF-ELSEIF-ELSE-STEAMClown.ino
@@ -0,0 +1,30 @@
+// Example Of “Ask A Question, Get 2 Answers/Choices” Program
+// Shows and IF / ELSE IF example
+int x = 7;
+
+void setup() {
+  Serial.begin(9600);           // Use Serial Monitor to debug
+  // Example of a simple IF statement. 
+  // Change the value of "x" above and rerun code
+  // it see the IF statement run
+  Serial.print("The value of x = "); 
+  Serial.println(x);    
+  if(x > 12)
+  {
+    Serial.print("The number "); 
+    Serial.print(x); 
+    Serial.println(" is bigger than 12");
+  }
+  else
+  {
+    Serial.print("The number "); 
+    Serial.print(x); 
+    Serial.println(" is less than or equal to 12");
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
