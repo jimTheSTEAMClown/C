dc9a58ff40621a3d981f6320e72f43ce8a8269ce
Jim Burnham <jim.the.steam.clown@gmail.com>
2019-09-09 11:03:55 -0700

    Create Arduino-IF-Simple.ino

---
+++ Arduino-IF-Simple.ino
@@ -0,0 +1,22 @@
+// Example “Ask A Question” Program
+int x = 44;
+
+void setup() {
+  Serial.begin(9600);           // Use Serial Monitor to debug
+  // Example of a simple IF statement. 
+  // Change the value of "x" above and rerun code
+  // it see the IF statement run
+  Serial.print("The value of x = "); 
+  Serial.println(x);    
+  if(x > 12)
+  {
+    Serial.print("The number "); 
+    Serial.print(x); 
+    Serial.println(" is bigger than 12");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
