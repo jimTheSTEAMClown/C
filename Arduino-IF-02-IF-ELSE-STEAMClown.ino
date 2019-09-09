30a8e0b5e2aedb4f062b5328ae3edbd4664239d0
Jim Burnham <jim.the.steam.clown@gmail.com>
2019-09-09 11:17:58 -0700

    Update Arduino-IF-02-IF-ELSE-STEAMClown.ino

--- Arduino-IF-02-IF-ELSE-STEAMClown.ino
+++ Arduino-IF-02-IF-ELSE-STEAMClown.ino
@@ -4,9 +4,9 @@ int x = 7;
 
 void setup() {
   Serial.begin(9600);           // Use Serial Monitor to debug
-  // Example of a simple IF statement. 
-  // Change the value of "x" above and rerun code
-  // it see the IF statement run
+  // Example of a simple IF / ELSE statement. 
+  // Change the value of "x" above and rerun code to see which of 
+  // the IF and ELSE statement based on what value of "x" is.
   Serial.print("The value of x = "); 
   Serial.println(x);    
   if(x > 12)
